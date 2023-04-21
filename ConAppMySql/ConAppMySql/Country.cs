97c988a4a068f676d30c72ed245696e895e9560f
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-04-20 23:59:56 -0700

    Commit ConAppMySql
    Commit ConAppMySql

---
+++ ConAppMySql/ConAppMySql/Country.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppMySql
+{
+	public class Country
+	{
+		public string Code { get; set; }
+		public string Name { get; set; }
+		public string Continent { get; set; }
+		public string Region { get; set; }
+		public decimal SurfaceArea { get; set; }
+		public short IndepYear { get; set; }
+		public int Population { get; set; }
+		public decimal LifeExpectancy { get; set; }
+		public decimal GNP { get; set; }
+		public decimal GNPOld { get; set; }
+		public string LocalName { get; set; }
+		public string GovernmentForm { get; set; }
+		public string HeadOfState { get; set; }
+		public int? Capital { get; set; }
+		public string Code2 { get; set; }
+
+		public Country() { }
+		public Country(string Code_, string Name_, string Continent_, string Region_, decimal SurfaceArea_, short IndepYear_, int Population_, decimal LifeExpectancy_, decimal GNP_, decimal GNPOld_, string LocalName_, string GovernmentForm_, string HeadOfState_, int Capital_, string Code2_)
+		{
+			this.Code = Code_;
+			this.Name = Name_;
+			this.Continent = Continent_;
+			this.Region = Region_;
+			this.SurfaceArea = SurfaceArea_;
+			this.IndepYear = IndepYear_;
+			this.Population = Population_;
+			this.LifeExpectancy = LifeExpectancy_;
+			this.GNP = GNP_;
+			this.GNPOld = GNPOld_;
+			this.LocalName = LocalName_;
+			this.GovernmentForm = GovernmentForm_;
+			this.HeadOfState = HeadOfState_;
+			this.Capital = Capital_;
+			this.Code2 = Code2_;
+		}
+	}
+}
---
+++ ConAppMySql/ConAppMySql/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ConAppMySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dataTable = new DataTable();
+            dataTable = ReadMySQL();
+            ReadRowCountry(dataTable);
+        }
+
+        public static DataTable ReadMySQL()
+        {
+            string connectionString = "server=localhost;database=world;uid=root;password=123456";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            string query = "SELECT * FROM country";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            DataTable dataTable = new DataTable();
+            using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command))
+            {
+                dataAdapter.Fill(dataTable);
+            }            
+            connection.Close();
+            return dataTable;
+        }
+
+        public static void ReadRowCountry(DataTable dataTable)
+        {
+            Country country = new Country();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                if (row.Field<int?>("Capital").Equals(null))
+                { country.Capital = 0; }
+                else
+                {
+                    country.Capital = row.Field<int?>("Capital");
+                }
+                country.Name = row.Field<string>("Name");
+                // Test Data 
+                Console.WriteLine("{0} {1}", country.Capital, country.Name);
+            }
+        }
+    }
+}
