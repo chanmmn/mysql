b06cb5484cec3750734b09c6f930225162fec721
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-06-11 16:04:14 +0800

    Commit ConAppMySqltoMSSQL
    Commit ConAppMySqltoMSSQL

---
+++ ConAppMySqltoMSSQL/ConAppMySqltoMSSQL/Models/PocContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConAppMySqltoMSSQL.Models;
+
+public partial class PocContext : DbContext
+{
+    public PocContext()
+    {
+    }
+
+    public PocContext(DbContextOptions<PocContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Actor> Actors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=poc;Trusted_Connection=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Actor>(entity =>
+        {
+            entity.ToTable("actor");
+
+            entity.Property(e => e.ActorId)
+                .ValueGeneratedNever()
+                .HasColumnName("actor_id");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(500)
+                .HasColumnName("first_name");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(500)
+                .HasColumnName("last_name");
+            entity.Property(e => e.LastUpdate)
+                .HasColumnType("datetime")
+                .HasColumnName("last_update");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ConAppMySqltoMSSQL/ConAppMySqltoMSSQL/Program.cs
@@ -0,0 +1,85 @@
+﻿using ConAppMySqltoMSSQL.Models;
+using Microsoft.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ConAppMySqltoMSSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            Console.WriteLine("Hello, Start");
+            dt = ReadMySQL();
+            ReadRowActor(dt);
+            Console.WriteLine("Hello, End");
+        }
+
+        public static DataTable ReadMySQL()
+        {
+            string connectionString = "server=localhost;database=poc;uid=root;password=password;convert zero datetime=True";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            //string query = "SELECT * FROM collections WHERE collect_date < '2023-03-01' AND collect_date > '2023-01-01' ORDER BY collect_date DESC";
+            string query = "SELECT * FROM actor";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            DataTable dataTable = new DataTable();
+            using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command))
+            {
+                dataAdapter.Fill(dataTable);
+            }
+            connection.Close();
+            return dataTable;
+        }
+
+        public static void ReadRowActor(DataTable dataTable)
+        {
+            int count = 0;
+            MyActor myactor = new MyActor();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                myactor.ActorId = row.Field<uint>("actor_id");
+                myactor.FirstName = row.Field<string>("first_name");
+                myactor.LastName = row.Field<string>("last_name");
+                if (!row.Field<DateTime?>("last_update").Equals(null))
+                {
+                    myactor.LastUpdate = row.Field<DateTime>("last_update");                    
+                }
+                InsertActor(myactor);
+                count++;                
+                Console.Write(count);
+            }
+            Console.WriteLine();
+        }
+
+        public static void InsertActor(MyActor myactor)
+        {
+            PocContext context = new PocContext();
+            Actor actor = new Actor();
+            actor.ActorId = myactor.ActorId; 
+            actor.FirstName = myactor.FirstName; 
+            actor.LastName = myactor.LastName;
+            if (myactor.LastUpdate < DateTime.Parse("1900-01-01"))
+            {
+                actor.LastUpdate = DateTime.Parse("1900-01-01");
+            }
+            else
+            {
+                actor.LastUpdate = DateTime.Parse(myactor.LastUpdate.ToString("yyyy-MM-dd"));
+            }
+            context.Actors.Add(actor);
+            context.SaveChanges();
+        }
+        public static void TestEF()
+        {
+            PocContext pocContext = new PocContext();
+            Actor actor = new Actor();
+            actor.ActorId = 1;
+            actor.FirstName = "Chan";
+            actor.LastName = "MM";
+            pocContext.Actors.Add(actor);
+            pocContext.SaveChanges();
+        }
+    }
+}
