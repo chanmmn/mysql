ee636ea86b117219e1a3eaee212a074781e0349a
chanmmn <33271651+chanmmn@users.noreply.github.com>
2025-04-11 23:50:55 -0700

    Commit readmysql.ps1

---
+++ 2025/powershellreadmysql/readmysql.ps1
@@ -0,0 +1,29 @@
+﻿Param(
+  [Parameter(
+  Mandatory = $true,
+  ParameterSetName = '',
+  ValueFromPipeline = $true)]
+  [string]$Query
+  )
+$MySQLAdminUserName = 'root'
+$MySQLAdminPassword = 'Pa$$w0rd'
+$MySQLDatabase = 'sakila'
+$MySQLHost = 'localhost'
+$ConnectionString = "server=" + $MySQLHost + ";port=3306;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";SslMode=none;database="+$MySQLDatabase
+Try {
+  [void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
+  $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection
+  $Connection.ConnectionString = $ConnectionString
+  $Connection.Open()
+  $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query, $Connection)
+  $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($Command)
+  $DataSet = New-Object System.Data.DataSet
+  $RecordCount = $dataAdapter.Fill($dataSet, "data")
+  $DataSet.Tables[0]
+  }
+Catch {
+  Write-Host "ERROR : Unable to run query : $query `n$Error[0]"
+ }
+Finally {
+  $Connection.Close()
+  }
